a7e6fd16aef901f84d91fd131c8ca6e9c6e99048
Moulika-21 <23b01a12c6@svecw.edu.in>
2025-05-28 12:39:37 +0530

    first commit

---
+++ sql.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+connection=sqlite3.connect("student.db") #connect to sqlite
+
+#create a  cursor object to insert record,create table,retrieve
+cursor=connection.cursor()
+
+table_info="""
+Create table STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),SECTION VARCHAR(25),MARKS INT);
+"""
+
+cursor.execute(table_info)
+
+#insert records
+cursor.execute('''Insert Into STUDENT values('Deepika','Data Science','A',90)''')
+cursor.execute('''Insert Into STUDENT values('Advaith','Machine Learning','B',100)''')
+cursor.execute('''Insert Into STUDENT values('Pragnya','Data Science','A',70)''')
+cursor.execute('''Insert Into STUDENT values('Nihan','Data Science','A',60)''')
+cursor.execute('''Insert Into STUDENT values('Pranuthi','Machine Learning','B',35)''')
+
+##Display all the records
+print("The inserted records are")
+
+data=cursor.execute('''Select * from STUDENT''')
+
+for row in data:
+    print(row)
+
+##close the connection
+connection.commit()
+connection.close()
